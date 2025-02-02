ddad555f78dcdc9484c83046b4443b07bf06e016
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-02 09:39:42 +0100

    text

--- script.js
+++ script.js
@@ -4,50 +4,51 @@ const email=document.getElementById("email");
 const textarea=document.getElementById("textarea");
 const form=document.querySelector("form");
 console.log(nameForm , surname , email  ,  textarea ,  form);
+const spanEmail=document.getElementById("spanEmail");
 const spanName=document.querySelector(".spanName");
 const spanSurname=document.querySelector(".spanSurname");
 const  spanTextarea=document.querySelector(".spanTextarea");
 const symbols = /[^a-zA-Z0-9\s]/;
 
 
-// nameForm.addEventListener("input",(e) =>{
-// const valueName=e.target.value;
-// console.log(valueName);
+nameForm.addEventListener("input",(e) =>{
+const valueName=e.target.value;
+console.log(valueName);
 
-// if(valueName===""){
-//     nameForm.classList.add("error");
-//     spanName.textContent="le nom est obligatoire"
-// }else if(valueName.length<3 || valueName.length>30){
-//     nameForm.classList.add("error");
-//     spanName.textContent="le nom doit être entre 3 et 30 caractères";
-// }
-// else if(symbols.test(valueName)){
-//     nameForm.classList.add("error");
-//     spanName.textContent="le nom ne peut contenir des caracteres speciaux";
-// }else{
-//     nameForm.classList.remove("error");
-//     spanName.textContent="";
-// }
-// })
-// surname.addEventListener("input",(e) =>{
-// const valueSurname=e.target.value;
-// console.log(valueSurname);
+if(valueName===""){
+    nameForm.classList.add("error");
+    spanName.textContent="le nom est obligatoire"
+}else if(valueName.length<3 || valueName.length>30){
+    nameForm.classList.add("error");
+    spanName.textContent="le nom doit être entre 3 et 30 caractères";
+}
+else if(symbols.test(valueName)){
+    nameForm.classList.add("error");
+    spanName.textContent="le nom ne peut contenir des caracteres speciaux";
+}else{
+    nameForm.classList.remove("error");
+    spanName.textContent="";
+}
+})
+surname.addEventListener("input",(e) =>{
+const valueSurname=e.target.value;
+console.log(valueSurname);
 
-// if(valueSurname===""){
-//     surname.classList.add("error");
-//     spanSurname.textContent="le nom est obligatoire"
-// }else if(valueSurname.length<3 || valueSurname.length>30){
-//     surname.classList.add("error");
-//     spanSurname.textContent="le nom doit être entre 3 et 30 caractères";
-// }
-// else if(symbols.test(valueSurname)){
-//     surname.classList.add("error");
-//     spanSurname.textContent="le nom ne peut contenir des caracteres speciaux";
-// }else{
-//    surname.classList.remove("error");
-//     spanSurname.textContent="";
-// }
-// })
+if(valueSurname===""){
+    surname.classList.add("error");
+    spanSurname.textContent="le nom est obligatoire"
+}else if(valueSurname.length<3 || valueSurname.length>30){
+    surname.classList.add("error");
+    spanSurname.textContent="le nom doit être entre 3 et 30 caractères";
+}
+else if(symbols.test(valueSurname)){
+    surname.classList.add("error");
+    spanSurname.textContent="le nom ne peut contenir des caracteres speciaux";
+}else{
+   surname.classList.remove("error");
+    spanSurname.textContent="";
+}
+})
 textarea.addEventListener("input",(e) =>{
 const valueTextarea=e.target.value;
 console.log(valueTextarea);
@@ -66,6 +67,26 @@ else if(symbols.test(valueTextarea)){
    textarea.classList.remove("error");
     spanTextarea.textContent="";
 }
+})
+
+
+form.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    if(nameForm.classList.contains("error") || surname.classList.contains("error") || textarea.classList.contains("error")){
+        alert("veuillez remplir tous les champs correctement");
+        return;
+    }
+  else{
+    alert("Formulaire envoyé avec succès");
+    form.reset();
+    nameForm.classList.remove("error");
+    surname.classList.remove("error");
+    textarea.classList.remove("error");
+    spanName.textContent="";
+    spanSurname.textContent="";
+    spanTextarea.textContent="";
+ 
+  }
 })
 
 
